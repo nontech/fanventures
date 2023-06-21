24377ef80002f1909e16b67ca5bed398408189ed
Mukesh Jaiswal <jais.mukesh@gmail.com>
2023-06-21 10:18:07 +0200

    Tailwind CSS package added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from "next/app";
 
+// These styles apply to every route in the application
+import '../globals.css'
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Component {...pageProps} />
