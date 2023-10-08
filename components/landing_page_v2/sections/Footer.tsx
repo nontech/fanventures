b10bcc8872c2a8992cecfe6e475a16be8cd1471d
Mukesh Jaiswal <jais.mukesh@gmail.com>
2023-10-08 18:53:14 +0200

    last commit before commiting directly to main

---
+++ components/landing_page_v2/sections/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center mt-20">
+      <section className="w-full h-40 rounded-lg">
+        <h1>Footer Area</h1>
+        
+        
+      </section>
+    </div>
+  );
+};
+
+export default Footer
